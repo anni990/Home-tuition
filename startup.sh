fbc35ef502d78c8adb41890c2828d66075c5c5b5
anni990 <aniket22mishra2004@gmail.com>
2025-06-17 19:19:37 +0530

    .

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - 
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list 
+apt-get update 
+ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
 cd /home/site/wwwroot
 pip install -r requirements.txt
 gunicorn --bind=0.0.0.0 --timeout 600 run:app
